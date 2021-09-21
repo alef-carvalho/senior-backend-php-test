ed6c7dcf6d3fe8f987f1de03158830dbba6bf7b8
alefcarvalho <alef.webdev@gmail.com>
2021-09-21 11:06:26 -0400

    wip: add notification service

--- app/Services/TransferNotificationService.php
+++ app/Services/TransferNotificationService.php
@@ -2,7 +2,40 @@
 
 namespace App\Services;
 
+use Exception;
+use App\Enums\TransferNotification;
+use Illuminate\Support\Facades\Http;
+
 class TransferNotificationService extends Service
 {
+
+    private string $endpoint;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->endpoint = config('services.transfers.notification');
+    }
+
+    /**
+     * Execute the service.
+     * @return bool
+     * @throws Exception
+     */
+    public function notify(): bool
+    {
+
+        $response = Http::get($this->endpoint);
+
+        if($response->successful())
+        {
+            throw new Exception("The notification service is offline.");
+        }
+
+        return $response->json("message") === TransferNotification::Success;
+
+    }
 
 }
