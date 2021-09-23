033263b76cf4773df83519601ab2c383cd638372
alefcarvalho <alef.webdev@gmail.com>
2021-09-22 22:44:17 -0400

    finalize test

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -19,18 +19,18 @@ class CustomerSeeder extends Seeder
 
         Customer::query()->firstOrCreate([
             "email"     => "customer.pf@test.com",
-            "cpf_cnpj"  => "748.821.040-91"
+            "cpf_cnpj"  => "518.283.740-24"
         ], [
-            "full_name" => "Fake Person",
+            "full_name" => "Fake PF Customer",
             "balance"   => 1000,
             "type"      => CustomerType::PF
         ]);
 
         Customer::query()->firstOrCreate([
             "email"     => "customer.pj@test.com",
-            "cpf_cnpj"  => "63.398.241/0001-50"
+            "cpf_cnpj"  => "17.576.739/0001-49",
         ], [
-            "full_name" => "Fake Company",
+            "full_name" => "Fake PJ Customer",
             "balance"   => 1000,
             "type"      => CustomerType::PJ
         ]);
